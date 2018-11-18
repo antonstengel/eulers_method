cd5ebe3e007c5f22f9b9559a5e18ea331b7b20d4
Anton Stengel <31658804+antonstengel@users.noreply.github.com>
2018-11-18 16:38:25 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,22 +2,23 @@ document.getElementById("input_button").addEventListener("click", function(){
     document.getElementById("test_text").innerHTML = "button was registered";
     
     var equation = document.getElementById("function_textfield").value;
+    var equationLowercase = equation.toLowerCase();
     var xInit = Number(document.getElementById("xInit_textfield").value);
     var yInit = Number(document.getElementById("yInit_textfield").value);
     var xFin = Number(document.getElementById("xFin_textfield").value);
     var stepNum = Number(document.getElementById("stepNum_textfield").value);
     var stepSize = (xFin-xInit)/stepNum;
     
-    var answer = euler(xInit, yInit, xFin, stepNum, stepSize, equation);
+    var answer = euler(xInit, yInit, xFin, stepNum, stepSize, equationLowercase);
     document.getElementById("output_text").innerHTML = answer;
     
     console.log("button was pressed");
     console.log(xInit);
 });
 
-function euler(x, y, xFin, stepNum, stepSize, equation){
+function euler(x, y, xFin, stepNum, stepSize, equationLowercase){
     for(var i = 0; i < stepNum; i++){
-        var slope = eval(equation);
+        var slope = eval(equationLowercase);
         y += stepSize*slope;
         x += stepSize;
         console.log("x: " + x + ", y: " + y);
